6c239978713f486dbb2d4cf5b5aeba4413834356
Eoin <oconne19@tcd.ie>
2019-01-30 23:59:39 +0000

    added comments

--- lud.py
+++ lud.py
@@ -2,46 +2,43 @@ import numpy as np
 from scipy import linalg
 import sys
 
-def LU_solve(A,b):
-    r = 0
-    size = len(A)
-    L = np.eye(size)
-    while r<(size-1):
-        if A[r][r]!=0:
-            for i in range(r+1,size):
+def LU_solve(A,b):#The function takes a matrix A and a vector b and returns the vector x, the solution to Ax=b
+    r = 0 #This variable holds the current row that is being considered, goes from 0 to n-1
+    size = len(A) #the number of rows in the matrix
+    while r<(size-1): #iterate through all rows
+        if A[r][r]!=0: #check if the pivot is non-zero
+            for i in range(r+1,size): #if it is non zero create L matrices for the column
                 if A[i][r]!=0:
-                    L_c = L_mat(r,i,size,A[i][r]/A[r][r])
-                    A = np.dot(L_c,A)
-                    L = np.dot(L_c,L)
-            r+=1
-        else:
-            ind = first_nonzero(A[:,r],r,size)
-            if ind == -1:
+                    L_c = L_mat(r,i,size,A[i][r]/A[r][r]) #this function calculates the L matrix for A[i][r]
+                    A = np.dot(L_c,A) #apply the row operation to A
+                    b = np.dot(L_c,b) #apply the row operation to b
+            r+=1 #move on to next row
+        else: #if there is no pivot need to permute rows
+            ind = first_nonzero(A[:,r],r,size) #this function returns the index of the first non-zero entry below the pivot in that column
+            if ind == -1: #if there is no non-zero entry then the matrix is singular
                 return ("unavoidable zero pivot")
-            P = perm_mat(ind,r,size)
+            P = perm_mat(ind,r,size)# this function calculates the permutation matrix to swap row r with the first row below with non-zero pivot
 
-            A = np.dot(P,A)
-            L = np.dot(P,L)
-    if abs(A[size-1][size-1])<10.0**(-10):
+            A = np.dot(P,A) #apply the permutation the A
+            b = np.dot(P,b) #apply the permutation the b
+    if abs(A[size-1][size-1])<10.0**(-10):#the final pivot of a singular matrix can often be non-zero due to rounding errors so if the final pivot is small enough claim that the matrix is singular
         return ("unavoidable zero pivot")
-    c = np.dot(L,b)
-    x = back_subs(A,c,size)
+    x = back_subs(A,b,size)# solve Ux=c by back substitution
     return x
 
 
-def L_mat(r,i,size,factor):
+def L_mat(r,i,size,factor): #this creates the L matrix for the gi
     L = np.eye(size)
-    L
     L[i][r] =  -factor
     return L
 
-def first_nonzero(col,r,size):
+def first_nonzero(col,r,size): #finds the first non-zero entry below the pivot
     for i in range(r,size):
         if col[i]!=0:
             return i
-    return -1
+    return -1 #if there is no non-zero entry return -1
 
-def perm_mat(i,r,size):
+def perm_mat(i,r,size): #this creates the permuation matrix to swap rows i and j
     P = np.eye(size)
     P[i][i] = 0
     P[r][r] = 0
@@ -49,7 +46,7 @@ def perm_mat(i,r,size):
     P[i][r] = 1
     return P
 
-def forward_subs(L,b,size):
+def forward_subs(L,b,size): #solves Lx=b for x by forward substitution
     x = np.zeros(size)
     x[0] = b[0]/L[0][0]
     for i in range(1,size):
@@ -60,7 +57,7 @@ def forward_subs(L,b,size):
     return x
 
 
-def back_subs(U,b,size):
+def back_subs(U,b,size): #solves Ux=b for x by backward substitution
     x = np.zeros(size)
     x[size-1] = b[size-1]/U[size-1][size-1]
     for i in range(1,size):
@@ -76,8 +73,7 @@ b = np.array([5,-2,6,9])
 
 print (LU_solve(A,b))
 lu, p = linalg.lu_factor(A)
-print(linalg.lu_solve((lu, p), b, True))
-print (linalg.solve(A,b))
+print(linalg.lu_solve((lu, p), b))
 print ("\n")
 
 A = np.array([[1,1,12,5],[1,3,1,4],[4,1,1,5],[2,4,5,7]])
@@ -85,8 +81,7 @@ b = np.array([5,-2,6,9])
 
 print (LU_solve(A,b))
 lu, p = linalg.lu_factor(A)
-print(linalg.lu_solve((lu, p), b, True))
-print (linalg.solve(A,b))
+print(linalg.lu_solve((lu, p), b))
 print ("\n")
 
 A = np.array([[2,1,1,2],[1,3,1,4],[1,1,6,5],[2,4,7,9]])
@@ -94,15 +89,13 @@ b = np.array([5,-2,6,9])
 
 print (LU_solve(A,b))
 lu, p = linalg.lu_factor(A)
-print(linalg.lu_solve((lu, p), b, True))
-print (linalg.solve(A,b))
+print(linalg.lu_solve((lu, p), b))
 print ("\n")
 
-A = np.array([[0,1,1,2],[0,1,1,4],[0,7,2,6],[1,1,7,9]])
+A = np.array([[0,1,1,2],[0,1,1,4],[0,7,2,6],[1,1,7,9]]) # this is an extra matrix to check that the permutations work
 b = np.array([5,-2,6,9])
 
 print (LU_solve(A,b))
 lu, p = linalg.lu_factor(A)
-print(linalg.lu_solve((lu, p), b, True))
-print (linalg.solve(A,b))
+print(linalg.lu_solve((lu, p), b))
 print ("\n")
